6c1de76aaa4965ced07a1ce5bbae271bd4d3484c
V-SpringT <vuxuanthinh2003@gmail.com>
2024-03-09 00:25:03 +0700

    .env config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+require('dotenv').config();
 const route = require('./routes/client/index.route.js');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("views","./views");
 app.set("view engine","pug");
