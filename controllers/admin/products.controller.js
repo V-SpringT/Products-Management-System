6c7e665b3ed045a1b0f235e175f8335a7cffecf5
V-SpringT <vuxuanthinh2003@gmail.com>
2024-03-14 23:56:25 +0700

    product filter feature

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -4,12 +4,47 @@ const Product = require("../../model/products.model");
 
 
 module.exports.index = async (req,res)=>{
-    const products = await Product.find({
+
+    let filterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Họat động",
+            status:"active",
+            class:""
+        },
+        {
+            name: "Dừng họat động",
+            status: "inactive",
+            class: ""
+        }
+    ]
+
+    if(req.query.status){
+        const index = filterStatus.findIndex(item => item.status == req.query.status)
+        filterStatus[index].class = "active"
+    }
+    else{
+        const index = filterStatus.findIndex(item => item.status == "")
+        filterStatus[index].class = "active"
+    }
+
+    let find = {
         deleted: false
-    });
+    }
+
+    if(req.query.status){
+        find.status = req.query.status
+    }
+    
+    const products = await Product.find(find);
 
     res.render("admin/page/products/index.pug",{
         pageTitle: "Trang san pham",
-        products: products
+        products: products,
+        filterStatus: filterStatus
     });
 }
