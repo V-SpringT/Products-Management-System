c829f646d912eeb09fba2f801787a2044265a9ca
V-SpringT <vuxuanthinh2003@gmail.com>
2024-04-04 23:20:02 +0700

    update cloud

--- routes/admin/products.route.js
+++ routes/admin/products.route.js
@@ -36,6 +36,7 @@ router.get("/edit/:id", controller.edit)
 router.patch(
     "/edit/:id", 
     upload.single('thumbnail'), 
+    uploadCloud.upload,
     validate.createPost,
     controller.editPatch
     )
